9368d79bc7a64695ebd647f217b41f2e7bcf925f
kimdh727 <kimdh_727@naver.com>
2020-03-17 00:45:35 +0900

    import

--- src/playground/Playground.scala
+++ src/playground/Playground.scala
@@ -50,25 +50,105 @@
 //  }
 //}
 
-package bobsdelights
+//package bobsdelights
+//
+//abstract class Fruit (val name: String, val color: String)
+//
+//object Fruits {
+//  object Apple extends Fruit("apple", "red")
+//  object Orange extends Fruit("orange", "orange")
+//  object Pear extends Fruit("pear", "yellowish")
+//  val menu = List(Apple, Orange, Pear)
+//}
+//
+//// easy access to Fruit
+//import bobsdelights.Fruit
+//
+//// easy access to all members of bobsdelights
+//import bobsdelights._
+//
+//// easy access to all members of Fruits
+//import bobsdelights.Fruit._
 
-abstract class Fruit (val name: String, val color: String)
+//class Outer {
+//  class Inner {
+//    private def f() = println("f")
+//
+//    class InnerMost {
+//      f()   // OK
+//    }
+//  }
+//  (new Inner).f() // error: f is not accessible
+//}
+//
+//package p {
+//  class Super {
+//    protected def f() = println("f")
+//  }
+//  class Sub extends Super {
+//    f()   // OK
+//  }
+//  class Other {
+//    (new Super).f() // error: f is not accessible
+//  }
+//}
 
-object Fruits {
-  object Apple extends Fruit("apple", "red")
-  object Orange extends Fruit("orange", "orange")
-  object Pear extends Fruit("pear", "yellowish")
-  val menu = List(Apple, Orange, Pear)
+package bobsrockets
+
+package navigation {
+  private[bobsrockets] class Navigator {
+    protected [navigation] def useStarChart() = {}
+    class LegOfJourney {
+      private[Navigator] val distance = 100
+    }
+    private[this] var speed = 200
+  }
+}
+package launch {
+//  import navigation._
+
+  object Vehicle {
+    private[launch] val guide = new Navigator
+  }
 }
 
-// easy access to Fruit
-import bobsdelights.Fruit
+class Rocket {
+  import Rocket.fuel
+  private def canGoHomeAgain = fuel > 20
+}
+object Rocket {
+  private def fuel = 10
+  def chooseStrategy(rocket: Rocket) = {
+    if (rocket.canGoHomeAgain)
+      goHome()
+    else
+      pickAStar()
+  }
+  def goHome() = {}
+  def pickAStar() = {}
+}
 
-// easy access to all members of bobsdelights
-import bobsdelights._
+// bobsdelights/package.scala
+package object bobsdelights {
+  def showFruit(fruit: Fruit) = {
+    import fruit._
+    println(name + "s are " + color)
+  }
+}
 
-// easy access to all members of Fruits
-import bobsdelights.Fruit._
+// PrintMenu.scala
+package printmenu
+improt bobsdelights.Fruits
+improt bobsdelights.showFruit
+object PrintMenu {
+  def main(args: Array[String]) = {
+    for (fruit <- Fruits.menu) {
+      showFruit(fruit)
+    }
+  }
+}
+
+
 
 object test extends App {
 
