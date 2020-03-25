7cb44112dd2678650f2818aa0ac821b50a63550e
kimdh727 <kimdh_727@naver.com>
2020-03-25 17:58:10 +0900

    udemy ch4

---
+++ src/lectures/part3fp/HandlingFailure.scala
@@ -0,0 +1,116 @@
+package lectures.part3fp
+
+import scala.util.control.{ControlThrowable, NonFatal}
+import scala.util.{Failure, Random, Success, Try}
+
+object HandlingFailure extends App {
+
+  // create success and failure
+  val aSuccess = Success(3)
+  val aFailure = Failure(new RuntimeException("SUPER FAILURE"))
+
+  println(aSuccess)
+  println(aFailure)
+
+  def unsafeMethod(): String = throw new RuntimeException("NO STRING FOR YOU BUSTER")
+
+  // Try objects via the apply method
+  val potentialFailure = Try(unsafeMethod())
+  println(potentialFailure)
+
+  // syntax sugar
+  val anotherPotentialFailure = Try {
+    // code that might throw
+    throw new RuntimeException("ANOTHER FAILURE")
+  }
+  println(anotherPotentialFailure)
+
+  // utilities
+  println(potentialFailure.isSuccess)
+  println(Try(1).isSuccess)
+  println(Try(throw new NoSuchElementException).isFailure)
+
+  // orElse
+  def backupMethod(): String = "A valid result"
+  val fallbackTry = Try(unsafeMethod()).orElse(Try(backupMethod()))
+  println(fallbackTry)
+
+  println(Try(unsafeMethod()) orElse Try(throw new RuntimeException))
+
+  // IF you design the API
+  def betterUnsafeMethod: Try[String] = Failure(new RuntimeException)
+  def betterBackupMethod: Try[String] = Success("A valid result")
+  val betterFallback = betterUnsafeMethod orElse betterBackupMethod
+
+  // map, flatMap, filter
+  println(aSuccess.map(_ * 2))
+  println(aSuccess.flatMap(x => Success(x * 10)))
+  println(aSuccess.filter(_ > 10))
+
+  val comp = Success(1)
+  val mapComp = comp.flatMap(a => Success(a + 1).filter(_ == 2).map(b => b * 10))
+  println(mapComp)
+  val forComp = for {
+    a <- comp
+    b <- Success(a + 1) if (b == 2)
+  } yield (b * 10)
+  println(forComp)
+
+  /**
+   *  Exercise
+   */
+
+  val host = "localhost"
+  val port = "8080"
+  def renderHTML(page: String) = println(page)
+
+  class Connection {
+    def get(url: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html>...</html>"
+      else throw new RuntimeException("Connection interrupted")
+    }
+
+    def getSafe(url: String): Try[String] = Try(get(url))
+  }
+
+  object HttpService {
+    val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection =
+      if (random.nextBoolean()) new Connection
+      else throw new RuntimeException("Someone else took the port")
+
+    def getSafeConnection(host: String, port: String): Try[Connection] = Try(getConnection(host, port))
+  }
+
+  // if you get the html page from the connection, print it to the console i.e. call renderHTML
+  val possibleConnection = HttpService.getSafeConnection(host, port)
+  val possibleHTML = possibleConnection.flatMap(connection => connection.getSafe("/home"))
+  possibleHTML.foreach(renderHTML)
+
+  // shorthand version
+  HttpService.getSafeConnection(host, port)
+    .flatMap(connection => connection.getSafe("/home"))
+    .foreach(renderHTML)
+
+  // for-comprehension version
+  for {
+    connection <- HttpService.getSafeConnection(host, port)
+    html <- connection.getSafe("/home")
+  } renderHTML(html)
+
+  /**
+   *  try {
+   *    connection = HttpService.getConnection(host, port)
+   *    try {
+   *      page = connection.get("/home")
+   *      renderHTML(page)
+   *    } catch (some other exception) {
+   *
+   *    }
+   *  } catch (exception) {
+   *
+   *  }
+   */
+}
